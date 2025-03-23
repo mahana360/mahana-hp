6c0e1577eb085f00e36a97970429247ee47b903a
suzu-shika <msz.006.ts@gmail.com>
2025-03-23 23:22:01 +0900

    サイトマップ設定

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,19 @@
 import { defineConfig } from 'astro/config';
+import robotsTxt from 'astro-robots-txt';
+import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://mahana-organic.com', // サイトのURL
+  integrations: [
+    sitemap(),
+    robotsTxt({
+      policy: [
+        {
+          userAgent: '*',
+          allow: '/',
+        },
+      ],
+      sitemap: 'https://mahana-organic.com/sitemap.xml', // サイトマップのURL
+    }),
+  ],
+});

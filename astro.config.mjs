54cb5f61a20393d6811c0606a3d8708530c5d273
suzu-shika <msz.006.ts@gmail.com>
2025-03-24 00:03:12 +0900

    robots.txt手動設定

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,7 @@
 import { defineConfig } from 'astro/config';
-import robotsTxt from 'astro-robots-txt';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://mahana-organic.com', // サイトのURL
-  integrations: [
-    sitemap(),
-    robotsTxt({
-      policy: [
-        {
-          userAgent: '*',
-          allow: '/',
-        },
-      ],
-      sitemap: 'https://mahana-organic.com/sitemap.xml', // サイトマップのURL
-    }),
-  ],
+  site: 'https://mahana-organic.com',
+  integrations: [sitemap()],
 });
